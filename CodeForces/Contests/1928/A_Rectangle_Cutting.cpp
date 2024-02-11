6ab67505c422408aaac17fd5e1a5f53830828cb3
r3kste <r3kst@proton.me>
2024-02-11 23:17:24 +0530

    add CF R 924

---
+++ CodeForces/Contests/1928/A_Rectangle_Cutting.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> ii;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+#define MOD (ll)(1e9 + 7)
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define sz(a) ((int) (a).size())
+
+#define rep(_, n) int _; for(_ = 0; _ < n; _++)
+
+#define in(_) cin >> _;
+#define in2(_0, _1) cin >> _0 >> _1;
+#define in3(_0, _1, _2) cin >> _0 >> _1 >> _2;
+#define vin(a) { rep (_, sz (a)) { in (a[_]) }}
+#define vvin(r, c) { rep(__,r) { rep(_,c) { in (matrix[__][_]) } } }
+#define br cout << "\n";
+#define out(_, __) cout << _ << __;
+#define o(_) out(_, " ")
+#define vout(__) for (auto _ : __) { o (_) } br
+#define vvout(___)  for (auto __ : ___) { vout (__); }
+
+#define oyes out("YES","\n")
+#define ono out("NO", "\n")
+
+int solve() {
+    fastio
+    ll a, b;
+    in2(a, b);
+    bool F = false;
+
+    if ((a % 2) == 0 && (b % 2) == 0) {
+        F = true;
+    } else if ((a % 2) == 0) {
+        if (b != a / 2) {
+            F = true;
+        }
+    } else if ((b % 2) == 0) {
+        if (a != b / 2) {
+            F = true;
+        }
+    }
+
+    if (F) {
+        o("Yes")br
+    } else {
+        o("No")br
+    }
+
+    return 0;
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    in (t);
+
+    while (t--) {
+        solve();
+    }
+}
